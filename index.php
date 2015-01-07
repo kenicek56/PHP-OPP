002f89dd180b3ea85c724a14621a70ac65c17f12
kenice washington <kenicek56@gmail.com>
2015-01-06 19:20:20 -0800

    made a third class still dont think im doing it right

--- index.php
+++ index.php
@@ -41,5 +41,42 @@ $kenice1­>age = "15";
 print "my name is"  {$kenice1­>name()}. {$kenice1­>age} "is my age."
 
 
+class Animals {
+	public $name= "linx";
+	public $gender="female";
+	public $age="7";
+	public $species="cat";
 
+	function meow() {
+     return "{$this->name}" .
+	"{$this->species}";
+
+	}
+
+	
+} 
+
+$cat1 = new meow();
+$cat1­>name = “linx”;
+$cat1­>species = “cat”;
+print “my favorite cat is  {$cat1­>name()}.”
+
+class pimp {
+	public $name= "kenice";
+	public $cane="canes";
+	public $hat="pimp hat";
+
+	function pimping() {
+     return "{$this->name}" .
+	"{$this->cane}";
+
+	}
+
+	
+} 
+
+$pimp1 = new pimping();
+$pimp1­>name = "kenice";
+$pimp1­>hat = "pimp hat";
+print "im a pimp named"  {$cat1­>name()}.
 ?>
