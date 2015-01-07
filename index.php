a453a4b2a1f1fd1520d1104b614e1542666d7952
kenice washington <kenicek56@gmail.com>
2015-01-06 19:08:15 -0800

    made my second class.

--- index.php
+++ index.php
@@ -19,6 +19,26 @@ $cat1­>name = “linx”;
 $cat1­>species = “cat”;
 print “my favorite cat is  {$cat1­>name()}.”
 
+class person {
+	public $name= "Kenice";
+	public $gender="female";
+	public $age="15";
+	public $species="human";
+
+	function code() {
+     return "{$this->name}" .
+	"{$this->age}";
+
+	}
+
+	
+} 
+
+$kenice1 = new code();
+$kenice1­>name = "kenice";
+$kenice1­>species = "human";
+$kenice1­>age = "15";
+print "my name is"  {$kenice1­>name()}. {$kenice1­>age} "is my age."
 
 
 
