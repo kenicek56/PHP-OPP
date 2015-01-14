efc57398f687390738204963dee0189c1903aafb
kenice washington <kenicek56@gmail.com>
2015-01-13 19:58:24 -0800

    yay i finished making three class examples!

--- inheritance.php
+++ inheritance.php
@@ -17,7 +17,7 @@ class School
 
 	function learning()
 	{
-		return "i hate " . $this->highschool;
+		return " hate " . $this->highschool;
 	}
 
 
@@ -29,6 +29,64 @@ class Highschool extends School {
 }
 
 $grades = new School("pasadena highschool", "sierra madre", "washington", "kindergarden");
-print"i love " . $highschool->learning();
+print" i attend, but " .$grades->learning();
+
+class Cookie {
+	public $chicken;
+	public $chocolatechip;
+	public $sugar;
+	public $blueberry;
+
+	function __construct($chicken, $chocolatechip, $sugar, $blueberry) {
+		$this->chicken = $chicken;
+		$this->chocolatechip = $chocolatechip;
+		$this->sugar = $sugar;
+		$this->blueberry = $blueberry;
+	}
+
+	function getName() {
+		return " and always will be " . $this->chocolatechip;
+	}
+}
+class Chocolatechip extends Cookie {
+	function chocolate() {
+		return $this->raisen;
+	}
+}
+
+$raisen = new Cookie("chiken", "chocolatechip", "sugar", "blueberry");
+print " my favorite cookie is ".$raisen->getName();
+
+class Mother
+{
+	public $cook;
+	public $work;
+	public $nag;
+	public $love;
+
+	function __construct($cook, $work, $nag, $love)
+	{
+		$this->cook = $cook;
+		$this->work = $work;
+		$this->nag = $nag;
+		$this->love = $love;
+	}
+
+	function yells()
+	{
+		return " my mom loves to " . $this->nag;
+	}
+
+
+}
+class Love extends Mother {
+	function teach() {
+		return $this->nag;
+	}
+}
+
+$teaches = new Mother("cook", "work", "nag", "love");
+print" i love my her but " .$teaches->yells();
 
 ?>
+
